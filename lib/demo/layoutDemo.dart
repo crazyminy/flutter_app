f54b16ba274352d806fce38829cf79d88bc088a2
crazyminy <crazyminy4131@gmail.com>
2019-04-21 20:10:49 +0800

    layoutDemo.dart

---
+++ lib/demo/layoutDemo.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+class LayoutDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+
+        ],
+      ),
+    );
+  }
+}
+
+class IconBadge extends StatelessWidget {
+  final IconData icon;
+  final double size;
+
+  IconBadge(this.icon, {this.size = 32.0});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Icon(
+        icon,
+        size: size,
+        color: Colors.white,
+      ),
+      width: size + 60,
+      height: size + 60,
+      color: Color.fromRGBO(3, 54, 255, 1.0),
+    );
+  }
+}
+
+class StackDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: Alignment.topLeft,
+      children: <Widget>[
+        SizedBox(
+          width: 200.0,
+          height: 200.0,
+          child: Container(
+            alignment: Alignment(0.5, 0.5),
+            decoration: BoxDecoration(
+              color: Colors.lightGreen,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Icon(
+              Icons.aspect_ratio,
+              color: Colors.white,
+              size: 32.0,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 50.0,
+          height: 50.0,
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.lightGreen,
+              borderRadius: BorderRadius.circular(8.0),
+            ),
+            child: Icon(
+              Icons.satellite,
+              color: Colors.white,
+              size: 32.0,
+            ),
+          ),
+        ),
+        Positioned(
+          right: 20.0,
+          child: Icon(
+            Icons.directions,
+            color: Colors.white,
+            size: 50.0,
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class AspectRatioDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 3.0 / 2.0,
+      child: Container(
+        color: Color.fromRGBO(3, 54, 255, 1.0),
+      ),
+    );
+  }
+}
+
+class ConstrainedBoxDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: BoxConstraints(
+          minHeight: 200.0,
+          maxWidth: 200.0
+      ),
+      child: Container(
+        color: Colors.lightBlue,
+      ),
+    );
+  }
+}

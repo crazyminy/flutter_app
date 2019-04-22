d5a9ba5c66416736d0eef1081e4524e2f4ba16de
crazyminy <crazyminy4131@gmail.com>
2019-04-22 22:31:26 +0800

    GridViewBuilder 按需生成视图

--- lib/demo/view_demo.dart
+++ lib/demo/view_demo.dart
@@ -3,6 +3,71 @@ import '../model/post.dart';
 
 class ViewDemo extends StatelessWidget {
 
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return GridViewBuilderDemo();
+  }
+}
+
+class GridViewBuilderDemo extends StatelessWidget {
+
+  Widget _gridItemBuilder(BuildContext context,int index){
+    return Container(
+      padding: EdgeInsets.all(8.0),
+      child: Image.network(
+        posts[index].imageUrl,
+        fit: BoxFit.cover,
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      itemCount: posts.length,
+      itemBuilder: _gridItemBuilder,
+      // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+      //   crossAxisCount: 3,
+      //   crossAxisSpacing: 8.0,
+      //   mainAxisSpacing: 8.0
+      // ),
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 150.0,
+      ),
+    );
+  }
+}
+
+class GridViewExtentDemo extends StatelessWidget {
+ List<Widget> _buildTiles(int length){
+    return List.generate(length, (int index){
+     return Container(
+        margin: EdgeInsets.all(8.0),
+        color: Colors.grey[300],
+        alignment: Alignment(0.0, 0.0),
+        child: Text(
+          'item $index',
+          style: TextStyle(fontSize: 18.0,color: Colors.pink[700]),
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+      maxCrossAxisExtent: 150.0,
+      crossAxisSpacing: 16.0,
+      mainAxisSpacing: 16.0,
+      scrollDirection: Axis.vertical,
+      children: _buildTiles(100),
+    );
+  }
+}
+
+class GridViewCountDemo extends StatelessWidget {
   List<Widget> _buildTiles(int length){
     return List.generate(length, (int index){
      return Container(
@@ -23,9 +88,8 @@ class ViewDemo extends StatelessWidget {
       crossAxisCount: 3,
       crossAxisSpacing: 16.0,
       mainAxisSpacing: 16.0,
-      children: <Widget>[
-
-      ],
+      scrollDirection: Axis.horizontal,
+      children: _buildTiles(100),
     );
   }
 }

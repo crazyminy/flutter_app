6d1cbf6a387dcf83245b251c319e4048ac6590c8
crazyminy <crazyminy4131@gmail.com>
2019-04-26 14:11:03 +0800

    create datetime demo

---
+++ lib/demo/datetime_demo.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateTimeDemo extends StatefulWidget {
+  @override
+  _DateTimeDemoState createState() => _DateTimeDemoState();
+}
+
+class _DateTimeDemoState extends State<DateTimeDemo> {
+
+  final DateTime selectedDate = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('_WidgetDemo'),),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                InkWell(
+                  onTap: (){},
+                  child: Row(
+                    children: <Widget>[
+                      Text(DateFormat.yMd().format(selectedDate)),
+                      Icon(Icons.arrow_drop_down)
+                    ],
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
